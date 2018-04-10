4d8dafa58978ba2e7303deb589ad43851953de3b
MidnightRabbit <taweesinw94@gmail.com>
2018-04-10 22:23:17 +0700

    Insert ComponentDidMount() in postcontainer

--- src/View/HomePage/PostComponents/postcontainer.js
+++ src/View/HomePage/PostComponents/postcontainer.js
@@ -9,7 +9,11 @@ class PostContainer extends Component {
     	super(props);
     	
     }
-    
+
+    componentDidMount()
+    {
+      var api = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=3&callback=/' 
+    }
   	render()
   	{
     	return (
